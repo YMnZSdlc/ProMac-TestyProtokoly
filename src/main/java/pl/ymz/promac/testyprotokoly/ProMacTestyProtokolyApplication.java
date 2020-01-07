afb69af73a59c305a07976327f14f72fc51dc5b3
Michał Mielczarek <michal.mielczarek@gmail.com>
2020-01-07 14:07:53 +0100

    edit .gitignore, add spring boot project

---
+++ src/main/java/pl/ymz/promac/testyprotokoly/ProMacTestyProtokolyApplication.java
@@ -0,0 +1,13 @@
+package pl.ymz.promac.testyprotokoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProMacTestyProtokolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProMacTestyProtokolyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ymz/promac/testyprotokoly/ProMacTestyProtokolyApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ymz.promac.testyprotokoly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProMacTestyProtokolyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
