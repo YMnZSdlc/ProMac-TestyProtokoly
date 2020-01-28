15081596332d46f33f0d1bbfe2021078bd51001c
YMnZSdlc <michal.mielczarek@gmail.com>
2020-01-28 05:54:19 +0100

    001

---
+++ src/main/java/pl/ymz/promac/testyprotokoly/model/User.java
@@ -0,0 +1,14 @@
+package pl.ymz.promac.testyprotokoly.model;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String pass;
+    private Boolean Admin;
+    private Boolean ReadOnly;
+
+
+
+}
