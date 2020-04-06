e1d871cf1f894b9ade742c84214cf022dc07b56e
Michał Mielczarek <michal.mielczarek@gmail.com>
2020-04-06 15:47:04 +0200

    temp

--- src/main/java/pl/ymz/promac/testyprotokoly/model/Product.java
+++ src/main/java/pl/ymz/promac/testyprotokoly/model/Product.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +19,11 @@ public class Product extends BaseEntity {
     private String productSN;           //numer seryjny produktu ze zlecenia
     private String productType;         //typ modułu np UPL710-2-63-ISO-12-B16
     private String productSymbol;       //oznaczenie rozdzielnicy jeśli istnieje
+
+    @OneToMany
     private List<Element> elements;     //lista elementów
+
+    @OneToMany
     private List<TestPosition> tests;   //lista testów
     private Long userId;                //osoba testująca produkt
     private Date testDate;              //data wykonania testu
